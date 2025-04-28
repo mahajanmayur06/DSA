1589af9ccb1db4cc75865ed63403c3dcea1f3dfc
Mayur <mah.mayur06@gmail.com>
2025-04-28 22:13:31 +0530

    added codeforces template

---
+++ template.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+#define printv(x) cout << #x << " is " << x << endl;
+#define printarr(c) { cout << #c << " = [ "; for (auto& element : c) { cout << element << " ";} cout << "]" << endl;}
+#define vi vector<int>
+#define f(i,a,b) for ( int i = a; i < b; i++)
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define no cout<<"NO"<<endl
+#define yes cout<<"YES"<<endl
+#define print(a) cout<<a<<endl
+
+void solve(){
+
+	int n;cin>>n;
+	vi arr(n);f(i,0,n)cin>>arr[i];
+
+
+}
+
+int main() {
+	std::ios::sync_with_stdio(false); std::cin.tie(NULL);
+	int t;cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+
+}
